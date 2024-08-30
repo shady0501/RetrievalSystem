d1ed4c88fbd8d951dbbf0c5e198b7c8ef730f19a
xpy <3135659122@qq.com>
2024-08-30 17:21:46 +0800

    文件下载、用户编辑功能加入头像图片下载功能

--- services/feedback_suggestion.py
+++ services/feedback_suggestion.py
@@ -7,7 +7,27 @@ from config import db_init as db
 # 用户反馈功能
 def feedback_submission(username,content):
     u = User.query.filter_by(username=username).first()
+    if u is None:
+        return jsonify({
+            'code': 0,
+            "message": "用户反馈成功",
+            "data": None
+        })
+
     user_id = u.id
+
+    # 获取用户的所有反馈记录
+    feedback_list = Feedback.query.filter_by(user_id=user_id).all()
+
+    # 遍历所有反馈记录，检查是否有内容与提交的内容相同
+    for feedback in feedback_list:
+        if feedback is not None and feedback.content.strip() == content.strip():
+            return jsonify({
+                'code': -10,
+                "message": "重复提交",
+                "data": None
+            })
+
     # status说明：pending-待处理状态; in_progress-正在处理中; resolved-问题已解决; closed-不再处理
     feedback = Feedback(user_id=user_id,content=content,date=datetime.now(timezone.utc),status="pending")
     try:
