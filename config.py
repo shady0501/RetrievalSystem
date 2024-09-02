58ddc8e2a3244a148c0dd9f7bee4ef7fc3534b97
xpy <3135659122@qq.com>
2024-09-02 15:32:44 +0800

    用户反馈记录、管理员获取用户数据接口完成，用户信息编辑功能改动

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 # Database configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@127.0.0.1:3306/retrievalsystem'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:110119XPY@127.0.0.1:3306/retrievalsystem'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Add this line to suppress warnings
 
 # Initialize the database object
