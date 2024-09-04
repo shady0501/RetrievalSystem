284a9ad2783ccd5d2001ee574a876f8c53f41879
xpy <3135659122@qq.com>
2024-09-04 10:12:51 +0800

    调用支付宝充值接口部分

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from flask_jwt_extended import JWTManager, create_access_token, jwt_required, ge
 app = Flask(__name__)
 
 # Database configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@127.0.0.1:3306/retrievalsystem'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:110119XPY@127.0.0.1:3306/retrievalsystem'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Add this line to suppress warnings
 
 # 生成一个安全的随机密钥
